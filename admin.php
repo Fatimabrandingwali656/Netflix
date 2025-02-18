468bc2a1c296c20b17e8555c4ccdf1f1f7760225
Fatimabrandingwali656 <zarwafatima27@gmail.com>
2025-02-18 16:29:05 +0500

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,8 @@
+<form method="POST" enctype="multipart/form-data">
+    <input type="text" name="title" placeholder="Movie Title" required><br>
+    <textarea name="description" placeholder="Movie Description" required></textarea><br>
+    <input type="text" name="category" placeholder="Category" required><br>
+    <input type="file" name="thumbnail" required><br>
+    <input type="file" name="video" required><br>
+    <button type="submit">Upload Movie</button>
+</form>
