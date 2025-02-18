31acf438aef0a2df208e628b25e6955a249a2f62
Fatimabrandingwali656 <zarwafatima27@gmail.com>
2025-02-18 16:29:29 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$conn = new mysqli('localhost', 'ungul0fkpz8rb', 'zadgznfwgquj', 'dbu2ituuxcibjw');
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
